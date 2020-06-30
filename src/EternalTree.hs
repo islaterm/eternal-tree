6e1aa6be8a00bbc236ae254de3a576ced99e68af
Ignacio Slater Muñoz <roac@Ignacio-PC.localdomain>
2020-06-30 15:24:47 -0400

    RC1: Implemented infinite tree v1.0.0-rc.1

--- src/EternalTree.hs
+++ src/EternalTree.hs
@@ -6,7 +6,7 @@
 -- You should have received a copy of the license along with this
 -- work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
 
--- v1.0.0-b.1
+-- v1.0.0-rc.1
 module EternalTree where
 
 data Tree a = Node a [Tree a] deriving Show
@@ -21,3 +21,9 @@ treeTake :: Int -> Int -> Tree a -> Tree a
 treeTake 0 _ (Node x _) = Node x []
 treeTake maxDepth maxLength (Node x ys) =
   Node x $ take maxLength $ map (treeTake (maxDepth - 1) maxLength) ys
+
+successors :: Int -> [Int]
+successors n = next : successors next where next = n + 1
+
+infTree :: Tree Int
+infTree = itTree successors 0
