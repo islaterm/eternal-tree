8e925881806e2a3e013d51a0e441f431fc5132d3
Ignacio Slater Muñoz <roac@Ignacio-PC.localdomain>
2020-07-02 13:31:54 -0400

    B3: Added tree transformation to Haskell Tree v1.0.0-b.3 - Added convertion to Haskell Tree - [TODO] Documentation

--- src/EternalTree.hs
+++ src/EternalTree.hs
@@ -6,24 +6,32 @@
 -- You should have received a copy of the license along with this
 -- work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
 
--- v1.0.0-rc.1
+-- v1.0.0-b.3
 module EternalTree where
 
-data Tree a = Node a [Tree a] deriving Show
+import           Data.Tree
+
+type Depth = Int
+type Children = Int
+data Tree' a = Node' a [Tree' a] deriving Show
 
 {-|
 Generates a tree with root r and where every child is f(r)
 -}
-itTree :: (a -> [a]) -> a -> Tree a
-itTree f r = Node r (map (itTree f) (f r))
+itTree :: (a -> [a]) -> a -> Tree' a
+itTree f r = Node' r (map (itTree f) (f r))
 
-treeTake :: Int -> Int -> Tree a -> Tree a
-treeTake 0 _ (Node x _) = Node x []
-treeTake maxDepth maxLength (Node x ys) =
-  Node x $ take maxLength $ map (treeTake (maxDepth - 1) maxLength) ys
+{-| Takes the first elements of a tree given a depth and a number of children per node. -}
+treeTake :: Depth -> Children -> Tree' a -> Tree' a
+treeTake 0 _ (Node' x _) = Node' x []
+treeTake depth children (Node' x ys) =
+  Node' x $ take children $ map (treeTake (depth - 1) children) ys
+
+toDataTree :: Tree' Int -> Tree String
+toDataTree (Node' v ts) = Node (show v) $ map toDataTree ts
 
 successors :: Int -> [Int]
 successors n = next : successors next where next = n + 1
 
-infTree :: Tree Int
+infTree :: Tree' Int
 infTree = itTree successors 0
